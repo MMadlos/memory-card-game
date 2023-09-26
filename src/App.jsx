da81119063bd20d248de4e697335331df307bc45
michael <michaelmadlos@hotmail.com>
2023-09-26 20:00:08 +0200

    Add fetched pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,45 @@ import Aside from "./components/Aside"
 import Card from "./components/Card"
 
 const API_URL = "https://pokeapi.co/api/v2/pokemon/"
-const DEFAULT_POKEMON_ID = 1
+const POKEMON_LIST_EASY = ["pikachu", "charmander", "dragonite", "squirtle", "bulbasaur", "gyarados", "psyduck", "snorlax", "mewtwo", "mew"]
 
 function App() {
-	const [pokemon, setPokemon] = useState("")
+	const [pokemonList, setPokemonList] = useState([])
 
 	// Fetch api
 	useEffect(() => {
-		fetch("https://pokeapi.co/api/v2/pokemon/1", { mode: "cors" })
-			.then((res) => res.json())
-			.then((pokemon) => {
-				const URL = pokemon.sprites.other["official-artwork"].front_default
-				setPokemon(URL)
-				console.log(URL)
-			})
+		function fetchPokemon(pokemonName) {
+			fetch(API_URL + pokemonName, { mode: "cors" })
+				.then((res) => res.json())
+				.then((pokemon) => {
+					const URL = pokemon.sprites.other["official-artwork"].front_default
+					const pokemonData = { name: pokemon.name, id: pokemon.id, url: URL }
+
+					setPokemonList((prev) => [...prev, pokemonData])
+				})
+		}
+
+		POKEMON_LIST_EASY.forEach((pokemon) => fetchPokemon(pokemon))
+
+		return () => {
+			const emptyArray = []
+			setPokemonList(emptyArray)
+		}
 	}, [])
 
 	return (
 		<>
 			<Aside />
 			<main id="card-list">
-				<Card pokemonURL={pokemon} />
-				{/* <Card />
-				<Card /> */}
+				{console.log(pokemonList)}
+				{pokemonList.map((pokemon) => {
+					return (
+						<Card
+							key={pokemon.id}
+							pokemonURL={pokemon.url}
+						/>
+					)
+				})}
 			</main>
 		</>
 	)
