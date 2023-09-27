4a01e770c0fba289b8b836b25c21623004c47c4b
michael <michaelmadlos@hotmail.com>
2023-09-27 17:26:56 +0200

    Add logic to get a defined number of random IDs

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,41 @@ const API_URL = "https://pokeapi.co/api/v2/pokemon/"
 const POKEMON_LIST_EASY = ["pikachu", "charmander", "dragonite", "squirtle", "bulbasaur", "gyarados", "psyduck", "snorlax", "mewtwo", "mew"]
 
 const getFethedURL = (data) => data.sprites.other["official-artwork"].front_default
-const getRandomNumber = (maxNumber) => Math.floor(Math.random() * maxNumber)
+
+function getRandomIntInclusive(min = 0, max) {
+	// The max and min are inclusive
+	min = Math.ceil(min)
+	max = Math.floor(max)
+	return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function getRandomUniqueIDs(numberOfIDs) {
+	let arrayWithIDs = []
+
+	for (let i = 0; i < numberOfIDs; i++) {
+		loopRandomNumsUntilFound()
+	}
+
+	function loopRandomNumsUntilFound() {
+		const randomNumber = getRandomIntInclusive(1, 151)
+		if (!arrayWithIDs.includes(randomNumber)) {
+			arrayWithIDs.push(randomNumber)
+			return
+		}
+		if (arrayWithIDs.includes(randomNumber)) loopRandomNumsUntilFound()
+	}
+
+	return arrayWithIDs
+}
 
 function App() {
 	const [pokemonList, setPokemonList] = useState([])
 	const [clickedPokemon, setClickedPokemon] = useState([])
 	const [currentPoints, setCurrentPoints] = useState(0)
 	const [maxPoints, setMaxPoints] = useState(0)
+
+	const mediumIDs = getRandomUniqueIDs(10)
+	console.log(mediumIDs)
 
 	useEffect(() => {
 		function fetchPokemon(pokemonName) {
@@ -38,8 +66,6 @@ function App() {
 	}, [])
 
 	function handleOnClick(e) {
-		randomizePokemonListOrder()
-
 		const { key } = e.target.dataset
 		const pokemonID = parseInt(key)
 
@@ -72,10 +98,10 @@ function App() {
 	function randomPokemonOrder() {
 		const currentPokemonList = pokemonList
 		const lengthPokemonList = pokemonList.length
-		let newArray = []
 
+		let newArray = []
 		for (let i = 0; i < lengthPokemonList; i++) {
-			const randomNumber = getRandomNumber(currentPokemonList.length)
+			const randomNumber = getRandomIntInclusive(currentPokemonList.length - 1)
 
 			newArray.push(currentPokemonList[randomNumber])
 			currentPokemonList.splice(randomNumber, 1)
